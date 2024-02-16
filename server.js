7b1fb1c899126dc0d12145e00dec02efad655c02
Mithlesh Sharma <itsyour.rohan@gmail.com>
2024-02-16 21:35:46 +0530

    production issue resolved

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ io.on("connection", (socket) => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
+    socket.emit("message", formatMessage(botName, "Welcome to Socketsphere!"));
 
     // Broadcast when a user connects
     socket.broadcast
